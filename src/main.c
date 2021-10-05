76c07b00f49d4b19c17629530b57aa32dbcb8b22
Denis Zvezdov <denis.zvezdov@ringteam.com>
2021-10-05 14:32:27 +0300

    corrected main.c

--- src/main.c
+++ src/main.c
@@ -1,43 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
 #include "bcm8544.h"
 
 
 void main(int argc, char* argv[]) {
 	int i;
-	int x = 0;
-	int y;
 	bcm8544_init();
-
 	clear_screen();
 	for (i=1; i<argc; i++) {
-		y = i - 1
-
-		if argv[i] != '-u' {
-			clear_screen();
-		} else {
-
-		}
-
-
+		clear_screen();
 		write_at(x, y, (argv[i]));
-
 	}
 }
-
-int main(int argc, char *argv[]){
-    int rez=0;
-    int x = 0;
-    int y = 0;
-
-
-//	opterr=0;
-	while ( (rez = getopt(argc,argv,"xy")) != -1){
-		switch (rez){
-		case 'x': printf("found argument \"x = %s\".\n",optarg); break;
-		case 'y': printf("found argument \"y = %s\".\n",optarg); break;
-		case '?': printf("Default\n");break;
-        };
-	};
-
-};
