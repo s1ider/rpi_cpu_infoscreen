b7c72c0b4b7c28a8bba0d3ff99e6fdca9485d69a
Denis Zvezdov <denis.zvezdov@ringteam.com>
2021-10-05 16:31:17 +0300

    enhanced main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include "bcm8544.h"
 
+#define USAGE "Usage: lcd_update TEXT [-xX] [-yY] [-u]\n"
 
-void main(int argc, char* argv[]) {
-	int i;
-	bcm8544_init();
-	clear_screen();
-	for (i=1; i<argc; i++) {
-		clear_screen();
-		write_at(x, y, (argv[i]));
+
+int main(int argc, char *argv[]){
+    int rez=0;
+    int x = 0;
+    int y = 0;
+    bool update = false;
+    char *msg;
+
+    bcm8544_init();
+
+	while ( (rez = getopt(argc,argv,"x:y:u")) != -1){
+		switch (rez){
+		case 'x':
+			x=atoi(optarg);
+			update = true;
+			break;
+		case 'y':
+			y=atoi(optarg);
+			update = true;
+			break;
+		case 'u':
+			update = true;
+			break;
+		case '?':
+			printf(USAGE);
+			break;
+        };
+	};
+
+	if (!update) clear_screen();
+
+	msg = argv[optind];
+ 	if (msg == NULL) {
+ 		printf("your TEXT is empty\n");
+		printf(USAGE);
+		return 1;
 	}
-}
+    // printf("message: %s at %i, %i\n", msg, x, y);
+    write_at(x, y, msg)
+
+    return 0;
+};
